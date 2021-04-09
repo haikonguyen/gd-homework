a86b1422038b7d06c4d82d9800440cb4fa9f7a32
Hai Ha Nguyen <haicorp87@gmai.com>
2021-04-09 16:56:23 +0200

    initial commit, adding repo based on create-gooddata-react-app gd-homework

---
+++ src/routes/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Page from "../components/Page";
+
+const Home: React.FC = () => {
+    return <Page>Place your content here Hello</Page>;
+};
+
+export default Home;
