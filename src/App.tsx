aad94f45a6cf9970acaea0f39d8f8d424019bab3
Hai Ha Nguyen <haicorp87@gmai.com>
2021-04-09 17:36:41 +0200

    adding eslint, prettier and tsconfig settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { BackendProvider } from "@gooddata/sdk-ui";
+import React, { FC } from 'react';
+import { BackendProvider } from '@gooddata/sdk-ui';
 
-import AppRouter from "./routes/AppRouter";
-import { useAuth } from "./contexts/Auth";
-import { WorkspaceListProvider } from "./contexts/WorkspaceList";
+import AppRouter from './routes/AppRouter';
+import { useAuth } from './contexts/Auth';
+import { WorkspaceListProvider } from './contexts/WorkspaceList';
 
-function App() {
-    const { backend } = useAuth();
+const App: FC = () => {
+  const { backend } = useAuth();
 
-    return (
-        <BackendProvider backend={backend}>
-            <WorkspaceListProvider>
-                <AppRouter />
-            </WorkspaceListProvider>
-        </BackendProvider>
-    );
-}
+  return (
+    <BackendProvider backend={backend}>
+      <WorkspaceListProvider>
+        <AppRouter />
+      </WorkspaceListProvider>
+    </BackendProvider>
+  );
+};
 
 export default App;
---
+++ src/components/FilterBar/FilterBar.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const FilterBar: FC = () => {
+  return <div>Filter bar goes here</div>;
+};
